67d7c5f90d7defe7c611487a76ff460a38924f90
jacobpradels <pradelsjacob@gmail.com>
2019-10-07 21:33:14 -0700

    end of night

--- undraw.py
+++ undraw.py
@@ -1,17 +1,20 @@
+nodes = []
 class Node:
-    def printchildren(node):
-        print('(')
-        print(node.name)
-        for child in node.children:
-            printchildren(child)
-        print(')')
-    def __init__(self, position, name, children=[],start,end):
+    
+    def __init__(self, y, x, name):
         self.self = self
-        self.position = position
-        self.children = children
+        self.y = y
+        self.x = x
+        self.children = []
         self.name = name
-        self.startpos = start
-        self.end = end
+        nodes.append(self)
+
+    def printchildren(self,child):
+        print('(',end="")
+        print(child.name,end="")
+        for child in child.children:
+            child.printchildren(child)
+        print(')',end="")
 
 inputCount = int(input())
 
@@ -24,6 +27,58 @@ def getTreeInput():
     
     return values
 
+# def getName(tree):
+#     print(tree)
+#     for x in range(0,len(tree),3):
+#         b = tree[x].split(" ")
+#         b = [x for x in b if x != ""]
+#         print(b)
 
-for x in range(inputCount):
+
+def getChildren(y,x,tree):
+    start = 0
+    end = 0
+    children = []
+    try:
+        if tree[y+1][x] == "|":
+            print('has children')
+            
+            offset = 0
+            print('here')
+            try:
+                while (tree[y+2][x-offset] == "-"):
+                    print('here')
+                    start = x - offset
+                    offset += 1
+            except IndexError:
+                start = 0
+            
+            offset = 0
+            '''
+            This is the broken area
+            HERE IT IS THOMAS
+            '''
+            while (tree[y+2][x+offset] == "-" and x + offset < len(tree)):
+                end = x + offset
+                offset += 1
+            for g in range(start,end+1):
+                if tree[y+3][g] != " ":
+                    print('got here')
+                    children.append(Node(y+3,g,tree[y+3][g]))
+            print("Start : %d, end : %d"%(start,end))
+    except IndexError:
+        3
+    return children
+
+for q in range(inputCount):
     tree = getTreeInput()
+    for y in range(len(tree))[0::3]:
+        # print(y)
+        for x in range(len(tree[y])):
+            if tree[y][x] != " ":
+                node1 = Node(y,x,tree[y][x])
+                # print('y : %d, x : %d'%(node1.y,node1.x))
+                kids = getChildren(node1.y,node1.x,tree)
+                for kid in kids:
+                    # node1.children.append(kid)
+                    print("KIDS NAME : ",kid.name)
