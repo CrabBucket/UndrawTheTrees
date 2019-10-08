7a3e26b35c6ffb754651b8f361d85ceffa5b1045
CrabBucket <tom@mcratracing.com>
2019-10-07 20:28:26 -0700

    Added princhildren

--- undraw.py
+++ undraw.py
@@ -1,4 +1,10 @@
 class Node:
+    def printchildren(node):
+        print('(')
+        print(node.name)
+        for child in node.children:
+            printchildren(child)
+        print(')')
     def __init__(self, position, name, children=[]):
         self.self = self
         self.position = position
